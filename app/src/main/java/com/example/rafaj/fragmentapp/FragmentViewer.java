649cb85acedd3cf9882fb183b01720a1f6dc8d7e
jossehblanco <jossehblanco@gmail.com>
2018-04-14 22:37:35 -0600

    Se agrego el codigo para mostrar la informacion del planeta en modo landscape

--- app/src/main/java/com/example/rafaj/fragmentapp/FragmentViewer.java
+++ app/src/main/java/com/example/rafaj/fragmentapp/FragmentViewer.java
@@ -5,33 +5,36 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 /**
  * Created by rafaj on 8/4/2018.
  */
 
 public class FragmentViewer extends Fragment {
-    TextView text;
+    TextView text,text2;
+    ImageView img;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.viewer_fragment, container, false);
 
         text = view.findViewById(R.id.textId);
+        text2 = view.findViewById(R.id.textId2);
+        img = view.findViewById(R.id.planetIMG);
         Bundle bundle = this.getArguments();
 
 
         if(bundle != null){
-            Toast.makeText(getActivity(), "Item: " + bundle.getString("KEY"), Toast.LENGTH_SHORT).show();
 
             text.setText(bundle.getString("KEY"));
+            text2.setText(bundle.getInt("info"));
+            img.setImageResource(bundle.getInt("imgRes"));
 
         }
 
         return view;
     }
-
-
 }
