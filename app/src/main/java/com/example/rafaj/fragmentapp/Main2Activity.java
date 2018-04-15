30c43307da3a007acc05457b74e2589c01b19a37
jossehblanco <jossehblanco@gmail.com>
2018-04-14 22:38:22 -0600

    Se agrego el codigo para mostrar la informacion del planeta en modo portrait

--- app/src/main/java/com/example/rafaj/fragmentapp/Main2Activity.java
+++ app/src/main/java/com/example/rafaj/fragmentapp/Main2Activity.java
@@ -1,37 +1,28 @@
 package com.example.rafaj.fragmentapp;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class Main2Activity extends AppCompatActivity {
-    TextView text;
-
+    TextView text, text2;
+    ImageView img;
+    Planet planet;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
         text = findViewById(R.id.textId);
-
-        Intent callingIntent = getIntent();
-        String intentAction = callingIntent.getAction();
-        String intentType = callingIntent.getType();
-
-        if (Intent.ACTION_SEND.equals(intentAction) && intentType != null){
-            if (intentType.equals("text/plain")){
-                handleReceivedText(callingIntent);
-            }
-        }
-
-    }
-
-    private void handleReceivedText(Intent intent){
-        String intentText = intent.getStringExtra(Intent.EXTRA_TEXT);
-
-        if (text != null){
-            text.setText(intentText);
-        }
+        text2 = findViewById(R.id.textId2);
+        img = findViewById(R.id.planetIMG);
+        Intent intent = getIntent();
+        planet = intent.getParcelableExtra("Holi");
+        text.setText(planet.planetName);
+        img.setImageResource(planet.imageRes);
+        text2.setText(planet.planetInfo);
     }
 }
